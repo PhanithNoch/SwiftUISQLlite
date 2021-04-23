02cf78494ab81356eb54879d947f5ac93d2a908d
nochphanith <nochphanith@gmail.com>
2021-04-23 11:54:29 +0700

    Initial Commit

---
+++ LocalStorageSwift/LocalStorageSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalStorageSwiftApp.swift
+//  LocalStorageSwift
+//
+//  Created by Admin on 4/23/21.
+//
+
+import SwiftUI
+
+@main
+struct LocalStorageSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
